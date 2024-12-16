69325837d221cc9965ec54dd9cb2e0b640238c99
Punchify Me <clementdes@users.noreply.github.com>
2024-12-16 12:07:05 +0100

    Create google_domains.py

---
+++ google_domains.py
@@ -0,0 +1,34 @@
+"""
+Configuration des domaines Google disponibles
+"""
+
+DOMAIN_OPTIONS = {
+    "Google Argentina": "google.com.ar",
+    "Google Australia": "google.com.au",
+    "Google Austria": "google.at",
+    "Google Belgium": "google.be",
+    "Google Brasil": "google.com.br",
+    "Google Canada": "google.ca",
+    "Google Chile": "google.cl",
+    "Google Colombia": "google.com.co",
+    "Google Denmark": "google.dk",
+    "Google Deutschland": "google.de",
+    "Google France": "google.fr",
+    "Google Greece": "google.com.gr",
+    "Google India": "google.co.in",
+    "Google Ireland": "google.ie",
+    "Google Italia": "google.it",
+    "Google Japan": "google.co.jp",
+    "Google Korea": "google.co.kr",
+    "Google Mexico": "google.com.mx",
+    "Google Nederland": "google.nl",
+    "Google Pakistan": "google.com.pk",
+    "Google Poland": "google.pl",
+    "Google Portugal": "google.pt",
+    "Google Spain": "google.es",
+    "Google Switzerland": "google.ch",
+    "Google Taiwan": "google.com.tw",
+    "Google Turkey": "google.com.tr",
+    "Google United Kingdom": "google.co.uk",
+    "Google USA": "google.com"
+}
